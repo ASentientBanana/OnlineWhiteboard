d110a2e3260e3c92c3e314c518b0e24dd074e23b
ASentientBanana <asentientbanana@gmail.com>
2020-09-07 02:49:15 +0200

    added sequalize

--- BE/main.ts
+++ BE/main.ts
@@ -4,9 +4,32 @@ let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 const cors = require('cors');
 const LoggerSingleton = require('./logger');
+const Sequalize = require('sequelize')
 
+const db = new Sequalize('whiteboardDB','postgres','lazy',{
+  host:'192.168.0.201',
+  // port:9465,
+  dialect:'postgres',
+  operatorsAliases:'false',
+  pool:{
+    max:5,
+    min:4,
+    acure:30000,
+    idle:10000
+  }
+});
 app.use(cors());
 app.use(express.json());
+
+
+ 
+//test
+db.authenticate().then(()=>{
+  console.log('db connected')
+}).catch((e:any)=>{
+  console.log(e);
+  
+})
 
 const rooms:any = {}
 io.on('connection', (socket:any) => {
